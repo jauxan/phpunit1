7e3318558b8d7acc4ec1f22c1679d5aa843d3e8d
jauxan <jauxan123@gmail.com>
2022-11-28 19:04:03 +0100

    Tercer commit - tests en varias edades

--- tests/unit/userTest.php
+++ tests/unit/userTest.php
@@ -9,4 +9,23 @@ public function testUserIsAnAdultReturnsFalseForMinors(){
 $user = new User(17);
 $this->assertEquals(false, $user->IsAnAdult());
 }
+
+public function testIsAnAdultReturnsTrueForPeopleOrderThanTreshold(){
+    $user = new User(19);
+    $this->assertTrue($user->isAnAdult());
+    
+    $user = new User(55);
+    $this->assertTrue($user->isAnAdult());
+    
+    $user = new User(102);
+    $this->assertTrue($user->isAnAdult());
+    
+    
+    }
+
+    public function testIsAnAdultReturnsTrueForTreshold(){
+        $user = new User(18);
+        $this->assertTrue($user->isAnAdult());
+    }
+
 }
